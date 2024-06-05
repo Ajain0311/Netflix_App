47d0724b3d100b400a17cc60766f8f0237ba2e9a
Aditya jain <113778106+Ajain0311@users.noreply.github.com>
2024-06-05 14:05:38 +0530

    Delete Users directory

--- Users/adity/OneDrive/Desktop/GITDEMO/Flutter_projects/gemini_chat_app_tutorial/android/app/src/main/kotlin/com/example/gemini_chat_app_tutorial/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.gemini_chat_app_tutorial
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- Users/adity/OneDrive/Desktop/GITDEMO/Flutter_projects/gemini_chat_app_tutorial/lib/consts.dart
+++
@@ -1,1 +0,0 @@
-const String GEMINI_API_KEY = "AIzaSyC73GLXA6pnxAQVgEB3c0Zqts8PPyf3goQ";
